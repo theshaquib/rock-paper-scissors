acfec7b6d7f4d371457a630e1f59b146295ce665
theshaquib <114143938+theshaquib@users.noreply.github.com>
2023-04-25 16:56:05 +0530

    testing

---
+++ app.js
@@ -0,0 +1,60 @@
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoice = document.querySelectorAll("button");
+
+let userChoice;
+let computerChoice;
+let result;
+
+possibleChoice.forEach((possibleChoice) =>
+  possibleChoice.addEventListener("click", function (e) {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice
+
+    generateComputerChoice()
+    getResult()
+  })
+);
+
+function generateComputerChoice(){
+  const randomNumber = Math.floor(Math.random() * possibleChoice.length) +1
+  
+
+  if (randomNumber === 1){
+    computerChoice = "rock"
+  }
+  if(randomNumber === 2){
+    computerChoice = "scissors"
+  }
+  if(randomNumber === 3 ){
+    computerChoice = "paper"
+  }
+  computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult(){
+  if (computerChoice === userChoice){
+    result = " its a draw!"
+  }
+  if (computerChoice === "rock" && userChoice === "paper"){
+    result = " you won!"
+  }
+  if (computerChoice === "rock" && userChoice === "scissors"){
+    result = " you lose!"
+  }
+  if(computerChoice === "paper" && userChoice === "scissore"){
+    result = " you won!"
+  }
+  if(computerChoice === "paper" && userChoice === "rock"){
+    result = " you lose!"
+  }
+  if (computerChoice === "scissors" && userChoice === "paper"){
+    result = " you lose!"
+  }
+  if (computerChoice === "scissors" && userChoice === "rock"){
+    result = " you won!"
+  }
+  resultDisplay.innerHTML = result
+}
+
